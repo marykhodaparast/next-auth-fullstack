ce79151205f732813b3d4cab082a5907eb0c1c93
marykhodaparast <maryam.kh1374@gmail.com>
2025-04-13 14:59:09 +0330

    try using i18next for changign lang to fa, but has issues

---
+++ next-i18next.config.js
@@ -0,0 +1,8 @@
+// next-i18next.config.js
+module.exports = {
+  i18n: {
+    defaultLocale: "fa",
+    locales: ["fa", "en"],
+    localeDetection: false,
+  },
+};
--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import React from "react";
 import SigninButton from "./SigninButton";
+import { useTranslation } from "react-i18next";
 
 const AppBar = () => {
+  const { t } = useTranslation();
   return (
     <header className="flex gap-4 p-4 bg-gradient-to-b from-white to-gray-200 shadow">
       <Link className="transition-colors hover:text-blue-500" href={"/"}>
-        Home Page
+        {/* {t('home')} */}
       </Link>
       <Link className="transition-colors hover:text-blue-500" href={"/UserPost"}>
-        User Post Page
+        {/* {t('user_post_page')} */}
       </Link>
       <SigninButton />
     </header>
